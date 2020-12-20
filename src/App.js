660a40706282f341236ba9f54983a5384eee102e
virendrawadher <68058974+virendrawadher@users.noreply.github.com>
2020-12-20 20:53:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./styles.css";
+import molist from "./database.js";
+
+export default function App() {
+  const [movieList, setMovieList] = useState("All");
+  // var listMovie = Object.keys(molist);
+  function clickHandler(buttons) {
+    setMovieList(buttons);
+  }
+
+  return (
+    <div className="App">
+      <h1>MoL!sT</h1>
+      <div className="movieBody">
+        <div className="movieBtn">
+          {Object.keys(molist).map(function (buttons) {
+            return (
+              <button
+                onClick={() => clickHandler(buttons)}
+                key={buttons}
+                className="movBtn"
+              >
+                {buttons}
+              </button>
+            );
+          })}
+        </div>
+        <div className="movieList">
+          {molist[movieList].map(function (movieL) {
+            return (
+              <div className="listOfMovie">
+                <div className="movies">
+                  <img
+                    src={movieL.thumnails}
+                    alt={movieL.movie_name}
+                    className="movieImg"
+                  ></img>
+                  <p className="movie movieName">{movieL.movie_name} </p>
+                  <p className="movie movieType">{movieL.type} </p>
+                  <p className="movie movieRate"> ⭐ {movieL.rating} </p>
+                  <p className="movie movieDesp">{movieL.description} </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/database.js
@@ -0,0 +1,119 @@
+const molist = {
+  All: [
+    {
+      movie_name: "Mank",
+      type: "R | 131 min | Biography, Comedy, Drama",
+      rating: 7.2,
+      description:
+        "1930's Hollywood is reevaluated through the eyes of scathing social critic and alcoholic screenwriter Herman J. Mankiewicz as he races to finish the screenplay of Citizen Kane (1941).",
+      thumnails:
+        "https://m.media-amazon.com/images/M/MV5BZjAzNDE2YzMtM2E4OC00NWViLTkwZTItYmVmYmNkYWNhOTMwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX67_CR0,0,67,98_AL_.jpg"
+    },
+    {
+      movie_name: "Big Mouth",
+      type: "TV-MA | 30 min | Animation, Comedy, Romance",
+      rating: 8.0,
+      description:
+        "Teenage friends find their lives upended by the wonders and horrors of puberty.",
+      thumnails:
+        "https://m.media-amazon.com/images/M/MV5BMzg0YzQzZTctOGM3OS00ZDE1LThiZDQtYzg4MTc1ZDUxZDM2XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX67_CR0,0,67,98_AL_.jpg"
+    },
+    {
+      movie_name: "The Mandalorian",
+      type: "TV-14 | 40 min | Action, Adventure, Sci-Fi",
+      rating: 8.7,
+      description:
+        "The travels of a lone bounty hunter in the outer reaches of the galaxy, far from the authority of the New Republic.",
+      thumnails:
+        "https://m.media-amazon.com/images/M/MV5BZDhlMzY0ZGItZTcyNS00ZTAxLWIyMmYtZGQ2ODg5OWZiYmJkXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_UX67_CR0,0,67,98_AL_.jpg"
+    },
+    {
+      movie_name: "Tenet",
+      type: "PG-13 | 150 min | Action, Sci-Fi",
+      rating: 7.6,
+      description:
+        "Armed with only one word, Tenet, and fighting for the survival of the entire world, a Protagonist journeys through a twilight world of international espionage on a mission that will unfold in something beyond real time.",
+      thumnails:
+        "https://m.media-amazon.com/images/M/MV5BYzg0NGM2NjAtNmIxOC00MDJmLTg5ZmYtYzM0MTE4NWE2NzlhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_UX67_CR0,0,67,98_AL_.jpg"
+    },
+    {
+      movie_name: "Attack on Titan",
+      type: "TV-MA | 24 min | Animation, Action, Adventure",
+      rating: 8.8,
+      description:
+        "After his hometown is destroyed and his mother is killed, young Eren Yeager vows to cleanse the earth of the giant humanoid Titans that have brought humanity to the brink of extinction.",
+      thumnails:
+        "https://m.media-amazon.com/images/M/MV5BMTY5ODk1NzUyMl5BMl5BanBnXkFtZTgwMjUyNzEyMTE@._V1_UX67_CR0,0,67,98_AL_.jpg"
+    },
+    {
+      movie_name: "Supernatural",
+      type: "TV-14 | 44 min | Drama, Fantasy, Horror",
+      rating: 8.4,
+      description:
+        "Two brothers follow their father's footsteps as hunters, fighting evil supernatural beings of many kinds, including monsters, demons and gods that roam the earth.",
+      thumnails:
+        "https://m.media-amazon.com/images/M/MV5BNzRmZWJhNjUtY2ZkYy00N2MyLWJmNTktOTAwY2VkODVmOGY3XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX67_CR0,0,67,98_AL_.jpg"
+    }
+  ],
+  Comedy: [
+    {
+      movie_name: "Mank",
+      type: "R | 131 min | Biography, Comedy, Drama",
+      rating: 7.2,
+      description:
+        "1930's Hollywood is reevaluated through the eyes of scathing social critic and alcoholic screenwriter Herman J. Mankiewicz as he races to finish the screenplay of Citizen Kane (1941).",
+      thumnails:
+        "https://m.media-amazon.com/images/M/MV5BZjAzNDE2YzMtM2E4OC00NWViLTkwZTItYmVmYmNkYWNhOTMwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX67_CR0,0,67,98_AL_.jpg"
+    },
+    {
+      movie_name: "Big Mouth",
+      type: "TV-MA | 30 min | Animation, Comedy, Romance",
+      rating: 8.0,
+      description:
+        "Teenage friends find their lives upended by the wonders and horrors of puberty.",
+      thumnails:
+        "https://m.media-amazon.com/images/M/MV5BMzg0YzQzZTctOGM3OS00ZDE1LThiZDQtYzg4MTc1ZDUxZDM2XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX67_CR0,0,67,98_AL_.jpg"
+    }
+  ],
+  Scifi: [
+    {
+      movie_name: "The Mandalorian",
+      type: "TV-14 | 40 min | Action, Adventure, Sci-Fi",
+      rating: 8.7,
+      description:
+        "The travels of a lone bounty hunter in the outer reaches of the galaxy, far from the authority of the New Republic.",
+      thumnails:
+        "https://m.media-amazon.com/images/M/MV5BZDhlMzY0ZGItZTcyNS00ZTAxLWIyMmYtZGQ2ODg5OWZiYmJkXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_UX67_CR0,0,67,98_AL_.jpg"
+    },
+    {
+      movie_name: "Tenet",
+      type: "PG-13 | 150 min | Action, Sci-Fi",
+      rating: 7.6,
+      description:
+        "Armed with only one word, Tenet, and fighting for the survival of the entire world, a Protagonist journeys through a twilight world of international espionage on a mission that will unfold in something beyond real time.",
+      thumnails:
+        "https://m.media-amazon.com/images/M/MV5BYzg0NGM2NjAtNmIxOC00MDJmLTg5ZmYtYzM0MTE4NWE2NzlhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_UX67_CR0,0,67,98_AL_.jpg"
+    }
+  ],
+  Horror: [
+    {
+      movie_name: "Attack on Titan",
+      type: "TV-MA | 24 min | Animation, Action, Adventure",
+      rating: 8.8,
+      description:
+        "After his hometown is destroyed and his mother is killed, young Eren Yeager vows to cleanse the earth of the giant humanoid Titans that have brought humanity to the brink of extinction.",
+      thumnails:
+        "https://m.media-amazon.com/images/M/MV5BMTY5ODk1NzUyMl5BMl5BanBnXkFtZTgwMjUyNzEyMTE@._V1_UX67_CR0,0,67,98_AL_.jpg"
+    },
+    {
+      movie_name: "Supernatural",
+      type: "TV-14 | 44 min | Drama, Fantasy, Horror",
+      rating: 8.4,
+      description:
+        "Two brothers follow their father's footsteps as hunters, fighting evil supernatural beings of many kinds, including monsters, demons and gods that roam the earth.",
+      thumnails:
+        "https://m.media-amazon.com/images/M/MV5BNzRmZWJhNjUtY2ZkYy00N2MyLWJmNTktOTAwY2VkODVmOGY3XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX67_CR0,0,67,98_AL_.jpg"
+    }
+  ]
+};
+export default molist;
